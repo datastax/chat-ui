bf09c428de1272c04e28e49c2ea01d5afa17db83
phact <estevezsebastian@gmail.com>
2024-02-15 11:53:23 -0500

    preprocess fix

--- svelte.config.vercel.js
+++ svelte.config.vercel.js
@@ -1,14 +1,29 @@
-// svelte.config.js
 import vercel from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import dotenv from "dotenv";
 
+dotenv.config({ path: "./.env.local" });
+dotenv.config({ path: "./.env" });
+
+process.env.PUBLIC_VERSION = process.env.npm_package_version;
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Your original config here
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: vercel(),
-    // rest of your config
+
+    paths: {
+      base: process.env.APP_BASE || "",
+    },
+    csrf: {
+      // handled in hooks.server.ts, because we can have multiple valid origins
+      checkOrigin: false,
+    },
   },
-  preprocess: preprocess(),
 };
 
 export default config;
